81dda2557cb5dfcfce1d5ce8cde4734fc6508e71
Prajjwal Datir <prajjwaldatir0072000@gmail.com>
2022-04-16 18:02:01 +0530

    Remember those old mobile phones with keypads?

---
+++ index.js
@@ -0,0 +1,38 @@
+export const convertTextToNumber = (text) => {
+  const mapping = {
+    'a': 2,
+    'b': 22,
+    'c': 222,
+    'd': 3,
+    'e': 33,
+    'f': 333,
+    'g': 4,
+    'h': 44,
+    'i': 444,
+    'j': 5,
+    'k': 55,
+    'l': 555,
+    'm': 6,
+    'n': 66,
+    'o': 666,
+    'p': 7,
+    'q': 77,
+    'r': 777,
+    's': 7777,
+    't': 8,
+    'u': 88,
+    'v': 888,
+    'w': 9,
+    'x': 99,
+    'y': 999,
+    'z': 9999
+  }  
+  let number = ''
+  for (let i = 0; i < text.length; i++) {
+    number += mapping[text[i]]
+  }
+  return number
+}
+
+// testing
+// console.log(convertTextToNumber('ola'))
